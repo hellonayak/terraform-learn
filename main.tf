9fc071e367992ba8348c0c3aaef21c8ea27061d6
Dibydarsi <ndibyadarsi@gmail.com>
2023-03-15 13:30:19 +0530

    initial commit

---
+++ main.tf
@@ -0,0 +1,36 @@
+provider "aws" {
+    region = "ap-south-1"
+ 
+}
+
+variable "cidr_blocks" {
+    description = "cidr blocks and name tags for vpc & subnets"  //this defines our varibale
+    type = list(object({
+        cidr_block = string
+        name = string
+    }))
+}
+
+resource "aws_vpc" "development-vpc" {
+    cidr_block = var.cidr_blocks[0].cidr_block      
+    tags = {
+        Name: var.cidr_blocks[0].name
+
+    }
+}
+
+resource "aws_subnet" "dev-subnet-1" {  
+    vpc_id = aws_vpc.development-vpc.id   
+    cidr_block = var.cidr_blocks[1].cidr_block
+    availability_zone = "ap-south-1a"
+     tags = {
+        Name: var.cidr_blocks[1].name
+    }
+}
+
+
+
+
+
+
+
